572316abd57433aa7e451b1cb2acd599b71b89a5
alastorid <alastorid@gmail.com>
2026-05-24 14:46:36 -0400

    Fix: Add Sankey diagram to R report and ensure networkD3 dependency is installed

--- scripts/render_report.R
+++ scripts/render_report.R
@@ -5,6 +5,7 @@ if (!require("dplyr")) install.packages("dplyr", repos="http://cran.rstudio.com/
 if (!require("ggplot2")) install.packages("ggplot2", repos="http://cran.rstudio.com/")
 if (!require("plotly")) install.packages("plotly", repos="http://cran.rstudio.com/")
 if (!require("jsonlite")) install.packages("jsonlite", repos="http://cran.rstudio.com/")
+if (!require("networkD3")) install.packages("networkD3", repos="http://cran.rstudio.com/")
 if (!require("tidyr")) install.packages("tidyr", repos="http://cran.rstudio.com/")
 
 rmarkdown::render("report.Rmd", output_file = "report.html")
