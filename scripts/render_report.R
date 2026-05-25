b194eb0152aedeff7f431b2469c8cdddda6f7e6b
alastorid <alastorid@gmail.com>
2026-05-24 21:59:58 -0400

    Fix data update workflow R dependencies

--- scripts/render_report.R
+++ scripts/render_report.R
@@ -1,11 +1,10 @@
 #!/usr/bin/env Rscript
-if (!require("rmarkdown")) install.packages("rmarkdown", repos="http://cran.rstudio.com/")
-if (!require("readr")) install.packages("readr", repos="http://cran.rstudio.com/")
-if (!require("dplyr")) install.packages("dplyr", repos="http://cran.rstudio.com/")
-if (!require("ggplot2")) install.packages("ggplot2", repos="http://cran.rstudio.com/")
-if (!require("plotly")) install.packages("plotly", repos="http://cran.rstudio.com/")
-if (!require("jsonlite")) install.packages("jsonlite", repos="http://cran.rstudio.com/")
-if (!require("networkD3")) install.packages("networkD3", repos="http://cran.rstudio.com/")
-if (!require("tidyr")) install.packages("tidyr", repos="http://cran.rstudio.com/")
+options(repos = c(RSPM = "https://packagemanager.posit.co/cran/__linux__/noble/latest"))
+
+packages <- c("rmarkdown", "readr", "dplyr", "ggplot2", "plotly", "jsonlite", "networkD3", "tidyr")
+missing <- packages[!vapply(packages, requireNamespace, logical(1), quietly = TRUE)]
+if (length(missing) > 0) {
+  install.packages(missing)
+}
 
 rmarkdown::render("report.Rmd", output_file = "report.html")
